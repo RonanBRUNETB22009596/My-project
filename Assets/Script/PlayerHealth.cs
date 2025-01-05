ba673b2006e85205da2dd9ab8d9d5a3fd6b2bdd7
ElSabri <sabri.khadraoui@etu.univ-amu.fr>
2025-01-05 19:45:02 +0100

    Modification - Menu
    Modification du Menu

--- Assets/Script/PlayerHealth.cs
+++ Assets/Script/PlayerHealth.cs
@@ -1,15 +1,19 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 100; // Santé maximale du joueur
     private int currentHealth; // Santé actuelle
+    public Slider healthBar; // Référence à la barre de vie dans le HUD
     public CharacterMovement characterMovement; // Référence au script CharacterMovement
     public DeathScreenController deathScreenController; // Référence au script DeathScreenController
 
     void Start()
     {
         currentHealth = maxHealth;
+        healthBar.maxValue = maxHealth; // Configurer la valeur maximale de la barre de vie
+        healthBar.value = currentHealth; // Initialiser la barre de vie
 
         if (characterMovement == null)
         {
@@ -20,6 +24,7 @@ public class PlayerHealth : MonoBehaviour
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;
+        healthBar.value = currentHealth; // Mettre à jour la barre de vie
         Debug.Log("Santé du joueur : " + currentHealth);
 
         if (currentHealth <= 0)
@@ -27,6 +32,7 @@ public class PlayerHealth : MonoBehaviour
             Die();
         }
     }
+
 
     void Die()
     {
